a63c0b3e6f7ddb37b374832718b417b0d102bda3
danielpm1982 <danielpm1982@gmail.com>
2020-03-14 14:05:18 -0300

    Create base project. closes #1

---
+++ src/main/java/com/danielpm1982/springboot2healthrecord/Springboot2HealthRecordApplication.java
@@ -0,0 +1,10 @@
+package com.danielpm1982.springboot2healthrecord;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2HealthRecordApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2HealthRecordApplication.class, args);
+    }
+}
---
+++ src/test/java/com/danielpm1982/springboot2healthrecord/Springboot2HealthRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielpm1982.springboot2healthrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2HealthRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
