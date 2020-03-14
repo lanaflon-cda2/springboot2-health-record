4c70ae44ce5cf598d718139edefbf4c31f7db033
danielpm1982 <danielpm1982@gmail.com>
2020-03-14 16:15:57 -0300

    Use and test all Controller methods, and application functionalities as a whole, from the Main class. Update Db class again to remove @Component (this class bean is configured at its @configuration java class not through @Component Annotations). closes #24

--- src/main/java/com/danielpm1982/springboot2healthrecord/Springboot2HealthRecordApplication.java
+++ src/main/java/com/danielpm1982/springboot2healthrecord/Springboot2HealthRecordApplication.java
@@ -1,10 +1,21 @@
 package com.danielpm1982.springboot2healthrecord;
+import com.danielpm1982.springboot2healthrecord.controller.PatientController;
+import com.danielpm1982.springboot2healthrecord.controller.ProfessionalController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Springboot2HealthRecordApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Springboot2HealthRecordApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(Springboot2HealthRecordApplication.class, args);
+        ProfessionalController professionalController = (ProfessionalController) ctx.getBean("professionalController");
+        professionalController.addProfessionalBootstrapDataLoad();
+        professionalController.showAllProfessionals();
+        PatientController patientController = (PatientController) ctx.getBean("patientController");
+        patientController.addPatientBootstrapDataLoad();
+        patientController.addConsultationBootstrapDataLoad();
+        patientController.showPatient(194878L);
+        patientController.showAllPatients();
     }
 }
